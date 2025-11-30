4e557950402a53ae707265ecd213eac0323f34d3
kiraaab <zS24013057@estudiantes.uv.mx>
2025-11-30 16:09:22 -0600

    Delete dominio directory

--- dominio/DireccionAgencia.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-@dataclass
-class DireccionAgencia:
-    calle: str
-    numero: str
-    cp: str
-    poblacion: str
--- dominio/DireccionVivienda.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-@dataclass
-class Direccionvivienda:
-    calle: str
-    numero: str
-    cp: str
-    poblacion: str
